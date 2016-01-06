676c09f2c6d9ec9b1617e178003dd1797a174863
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 12:06:57 -0800

    Fixed the cube function

--- arithmetic.py
+++ arithmetic.py
@@ -19,12 +19,14 @@ def divide(num1, num2):
     return float(num1) / num2    
 
 def square(num1):
-    """Returns the square of the input integers."""
+    """Returns the square of the input integer."""
 
     return num1 ** 2
     
 def cube(num1):
-    pass
+    """Returns the cube of the input integer."""
+
+    return num1 ** 3
 
 def power(num1, num2):
     pass
